9b319a806d36be2ea2eca8bad85bff0e03ce784f
Rajan Palaniya <razorpaul@gmail.com>
2024-05-20 09:49:49 +0530

    op7: fix script

--- anykernel.sh
+++ anykernel.sh
@@ -48,69 +48,8 @@ else
 fi
 ui_print " " "You are on $os_string!";
 
-if [ $os == "custom" ]; then
-  if [ -f $home/Image ]; then
-    mv $home/Image $home/Image-dtb;
-  fi;
-  if [ -f $home/dtb ]; then
-    cp $home/*dtb .;
-  fi;
-fi;
-
 ## AnyKernel install
 dump_boot;
-
-# Override DTB
-if [ $os == "stock" ]; then
-  mv $home/dtb $home/split_img/;
-fi
-
-# Move resetprop_static
-cp -rfp $home/resetprop_static /data/local/tmp/resetprop_static;
-chmod 755 /data/local/tmp/resetprop_static;
-
-if mountpoint -q /data; then
-  # Optimize F2FS extension list (@arter97)
-  for list_path in $(find /sys/fs/f2fs* -name extension_list); do
-    hash="$(md5sum $list_path | sed 's/extenstion/extension/g' | cut -d' ' -f1)"
-
-    # Skip update if our list is already active
-    if [[ $hash == "43df40d20dcb96aa7e8af0e3d557d086" ]]; then
-      echo "Extension list up-to-date: $list_path"
-      continue
-    fi
-
-    ui_print " " "Optimizing F2FS extension list..."
-    echo "Updating extension list: $list_path"
-
-    echo "Clearing extension list"
-
-    hot_count="$(grep -n 'hot file extens' $list_path | cut -d':' -f1)"
-    list_len="$(cat $list_path | wc -l)"
-    cold_count="$((list_len - hot_count))"
-
-    cold_list="$(head -n$((hot_count - 1)) $list_path | grep -v ':')"
-    hot_list="$(tail -n$cold_count $list_path)"
-
-    for ext in $cold_list; do
-      [ ! -z $ext ] && echo "[c]!$ext" > $list_path
-    done
-
-    for ext in $hot_list; do
-      [ ! -z $ext ] && echo "[h]!$ext" > $list_path
-    done
-
-    echo "Writing new extension list"
-
-    for ext in $(cat $home/f2fs-cold.list | grep -v '#'); do
-      [ ! -z $ext ] && echo "[c]$ext" > $list_path
-    done
-
-    for ext in $(cat $home/f2fs-hot.list); do
-      [ ! -z $ext ] && echo "[h]$ext" > $list_path
-    done
-  done
-fi
 
 # Install the boot image
 write_boot;
